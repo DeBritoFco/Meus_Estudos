cbd23f2610542099f0c1acff5b1e5fc15ebf5ace
debritofco <debritofco@gmail.com>
2022-10-06 19:47:51 -0300

    Playlist - Lógica de Programação em Ruby

---
+++ aprendendo-ruby/cookbook.rb
@@ -0,0 +1,61 @@
+puts 'Bem-vindo ao Cookbook, sua rede social de receitas'
+
+receitas = []
+
+puts '[1] Cadastrar receita'
+puts '[2] Ver todas as receitas'
+puts '[3] Sair'
+
+print 'Escolha uma opção: '
+opcao = gets.to_i() #valor de gets é um texto e não um número e nesse caso precisa transformar
+
+while (opcao != 3) do #infinito diferente do for que é finito: já nasce sabendo quantas vezes ele vai rodar. O do também é opcional
+
+    if(opcao == 1)
+        puts 'Digite o nome da receita: '
+        nome = gets.chomp()
+
+        receitas << nome
+        puts
+        puts "Receita #{nome} cadastrada com sucesso" #puts 'Receita ' + nome + ' cadastrada com sucesso !'
+        puts
+    elsif(opcao == 2)
+        puts '=========================== Receitas Cadastradas ==========================='
+
+        puts receitas
+
+        # for receita in receitas do --> exitem outros métodos em ruby que são mais usuais
+        #     puts receita
+
+        # end
+
+        # receitas.each do |receita|
+        #     puts receita
+        # end
+        puts
+    else 
+        puts 'Opção inválida'
+    end
+    puts '[1] Cadastrar receita'
+    puts '[2] Ver todas as receitas'
+    puts '[3] Sair'
+
+    print 'Escolha uma opção: '
+    opcao = gets.to_i()
+end
+
+puts 'Obrigado por usar o Cookbook, até logo'
+
+            
+
+
+
+
+
+
+#3. Array - lista ordenada / Pode qualquer tipo de dado em Ruby e incomum em outras linguagens
+#4. Estruturas de Repetição
+#5. Estruturas Condicionais
+
+
+
